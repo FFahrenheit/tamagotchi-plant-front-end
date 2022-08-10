28831881b98accbd46c88a2931106d4d1857a278
Iván L. Murillo <diskman199@gmail.com>
2022-08-09 23:49:52 -0500

    Recover password component added
    * Won't be used, but is good to have

--- src/app/auth/recover-password/recover-password.component.ts
+++ src/app/auth/recover-password/recover-password.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-recover-password',
+  selector: 'recover-password',
   templateUrl: './recover-password.component.html',
   styleUrls: ['./recover-password.component.scss']
 })
 export class RecoverPasswordComponent implements OnInit {
 
-  constructor() { }
+  @Output() public accept = new EventEmitter<void>();
+  @Output() public cancel = new EventEmitter<void>();
+  @Output() public reject = new EventEmitter<void>();
+  @Output() public triggered = new EventEmitter<void>();
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  open(content: any) {
+    this.triggered.emit();
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      switch (result) {
+        case 'YES':
+          this.accept.emit();
+          break;
+        case 'NO':
+          this.reject.emit();
+          break;
+        default:
+          this.cancel.emit();
+          break;
+      }
+    }, (reason) => {
+      this.cancel.emit();
+    });
   }
 
 }
