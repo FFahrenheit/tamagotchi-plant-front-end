cd9cfb0460a2e86513e694765095b9ead6d08752
Andres <andres@Andress-MacBook-Air.local>
2022-09-25 23:33:06 -0500

    chart bootstrapping

--- src/app/plants/plant-card/plant-card.component.ts
+++ src/app/plants/plant-card/plant-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plant-card',
@@ -7,9 +7,16 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class PlantCardComponent implements OnInit {
 
+  @Input() plantData:any;
+  temperatura = 0;
+  luminosidad = 0;
+  humedad_tierra = 0;
+  humedad_ambiente = 0;
+
   spinnerDiameter = window.innerHeight / 10;
 
   constructor() {
+
   }
 
   @HostListener('window:resize', ['$event'])
@@ -18,6 +25,13 @@ export class PlantCardComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log(this.plantData);
+    if (this.plantData) {
+      this.temperatura = this.plantData.last_rec.temperatura;
+      this.luminosidad = this.plantData.last_rec.luminosidad;
+      this.humedad_tierra = this.plantData.last_rec.humedad_tierra;
+      this.humedad_ambiente = this.plantData.last_rec.humedad_ambiente;
+    }
   }
 
 }
